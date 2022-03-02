4a2f221623ffbb5e9a73b59ce118ac6127f334d9
Robert <r.walerczak@gmail.com>
2022-03-02 12:51:15 +1100

    Add necessary code to run server.js

--- database/db.js
+++ database/db.js
@@ -4,19 +4,27 @@
 //     database: 'ok_bloomer',
 // });
 
+// let db;
+// if (process.env.NODE_ENV === 'production') {
+//     db = new pg.Pool({
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: {
+//             rejectUnauthorized: false,
+//         },
+//     });
+// } else {
+//     db = new pg.Pool({
+//         database: 'okay_bloomer',
+//         // password: 'optional_password', // If you have a password on your local db
+//     });
+// }
+
 // module.exports = db;
 
-let db;
-if (process.env.NODE_ENV === 'production') {
-    db = new pg.Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    });
-} else {
-    db = new pg.Pool({
-        database: 'okay_bloomer',
-        // password: 'optional_password', // If you have a password on your local db
-    });
-}
+const pg = require('pg');
+
+const db = new pg.Pool({
+    database: 'ok_bloomer',
+});
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-const express = require('express');
-const pg = require('pg');
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
-const port = process.env.PORT || 3000;
+const express = require('express');
+const expressSession = require('express-session');
+const pgSession = require('connect-pg-simple')(expressSession);
+const db = require('./database/db');
+
+const usersController = require('./controllers/users');
+
+// const errorHandler = require('./middleware/error_handler');
+// const logger = require('./middleware/logger');
+
+// const pg = require('pg');
+
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(
+    expressSession({
+        store: new pgSession({
+            pool: db, // Connects to our postgres db
+            createTableIfMissing: true, // Creates a session table in your database (go look at it!)
+        }),
+        secret: process.env.SESSION_SECRET, // Needs a secret key to keep session data secure
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+// app.use(logger);
+app.use(express.json());
+app.use(express.static('client'));
 
 app.get('/', (req, res) => {
     res.send('hello');
 });
+
+app.use('/api/users', usersController);
+// app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`server listening on port: ${port}`);
