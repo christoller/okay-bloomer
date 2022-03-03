bed3a164888f0803cb2393d18c38bd52be985b47
marc <marcpllew@gmail.com>
2022-03-03 23:04:54 +1100

    search results test not to commit

--- client/js/components/search-results.js
+++ client/js/components/search-results.js
@@ -2,7 +2,6 @@ function renderSearchResults(searchQuery) {
     axios.get(`/api/plants?search=${searchQuery}`).then((response) => {
         const results = response.data;
         const resultsContainer = document.createElement('div');
-
         results.forEach((result) => {
             const item = document.createElement('h3');
             item.innerHTML = `
