f26e9499a4526a138a610505651c268d4555e65d
kayta <kaytahackman@gmail.com>
2022-03-04 15:02:30 +1100

    fleshed out search results data, added functionality that clears prev results

--- client/js/components/search-results.js
+++ client/js/components/search-results.js
@@ -1,14 +1,20 @@
+const resultsContainer = document.createElement('section');
+
 function renderSearchResults(searchQuery) {
+    resultsContainer.innerHTML = '';
     axios.get(`/api/plants?search=${searchQuery}`).then((response) => {
         const results = response.data;
-        const resultsContainer = document.createElement('div');
 
         results.forEach((result) => {
-            const item = document.createElement('h3');
+            const item = document.createElement('div');
             item.innerHTML = `
-                <h3 onclick="renderPlant(${result.id})">
-                    ${result.name}
-                </h3>
+                <div id="search-result" onclick="renderPlant(${result.id})">
+                    <h3> ${result.name} </h3>
+                        <img src="${result.image_url}" height=200px width=200px ></img>
+                        <p>Latin name: <i> ${result.latin_name} </i></p>
+                        <p> ${result.description}</p>
+                        <a href="#####">Learn more about ${result.name} here</a>
+                </div>
             `;
             resultsContainer.appendChild(item);
         });
