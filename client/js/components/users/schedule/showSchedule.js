6b8c29f7452f1f2f9c9ec7e5927fb21d2e04b8e2
christoller <tollernz@gmail.com>
2022-03-05 14:30:28 +1100

    Add to schedule functionality

--- client/js/components/users/schedule/showSchedule.js
+++ client/js/components/users/schedule/showSchedule.js
@@ -5,23 +5,13 @@ function renderSchedule() {
 
     axios.get(`/api/schedule/`).then((response) => {
         const results = response.data;
-        // Create empty results array
-        const resultsArray = [];
 
         results.forEach((result) => {
-            // calculate when each action is due
-            // if less than x, append to resultsArray
-        });
-
-        // For each result in results array
-        resultsArray.forEach((result) => {
             const item = document.createElement('div');
 
             item.innerHTML = `
                 <div class="schedule-result">
-                    <h3> ${result.name} </h3>
-                        <img src="${result.image_url}" height=200px width=200px ></img>
-                        <p>This needs to be watered in ${result.watering_frequency_in_days} days</p>
+                    <p> ${result.plant.nickname} needs ${result.action} on ${result.dueDate} </p>       
                 </div>
             `;
             resultsContainer.appendChild(item);
