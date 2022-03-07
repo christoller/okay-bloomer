a3777797f008d4ee4df222e17562b1e83ce6aaee
christoller <tollernz@gmail.com>
2022-03-07 21:44:03 +1100

    integrate Tailwind into plant-detail page and search-results

--- client/js/components/plants/plant-details.js
+++ client/js/components/plants/plant-details.js
@@ -6,21 +6,24 @@ function renderPlant(id) {
     axios.get(`/api/plants/${id}`).then((response) => {
         const plant = response.data;
         let plantDiv = document.createElement('div');
+
         detail.appendChild(plantDiv).innerHTML = `
-            
-            <div><img src="${plant.image_url}" ></img></div>
-            <h3>${plant.name}</h3>
-            <div>${plant.latin_name}</div>
-            <div>${plant.description}</div>
-            <div>${plant.watering_frequency_in_days}</div>
-            <div>${plant.sunlight}</div>
-            <div>${plant.plant_location}</div>
-            <div>${plant.fertilising_frequency_in_days}</div>
-            <div>${plant.pruning_frequency_in_days}</div>
-            <div>${plant.repotting_frequency_in_days}</div>
-            <div>${plant.repotting_frequency_in_days}</div>
-            <div>${plant.soil_type}</div>
-            <button id="add-plant-to-schedule">Add to Schedule</button>
+            <div id="plant-details-container" class="bg-white rounded-lg w-9/12 mt-6 mx-auto mb-9 flex flex-wrap gap-6 p-8 shadow-lg shadow-black-500/40">
+                <img src="${plant.image_url}" class="w-1/4 h-1/4 "></img>
+                <div id="plant-details" class="flex flex-col gap-1">
+                <h3><span class="font-bold">Name:</span> ${plant.name}</h3>
+                <div><span class="font-bold">Latin Name:</span> ${plant.latin_name}</div>
+                <div><span class="font-bold">Watering Frequency:</span> Every ${plant.watering_frequency_in_days} Days</div>
+                <div><span class="font-bold">Sunlight:</span> ${plant.sunlight}</div>
+                <div><span class="font-bold">Indoors/Outdoors:</span> ${plant.plant_location}</div>
+                <div><span class="font-bold">Fertilising Frequency:</span> Every ${plant.fertilising_frequency_in_days} days</div>
+                <div><span class="font-bold">Pruning Frequency:</span> Every ${plant.pruning_frequency_in_days} days</div>
+                <div><span class="font-bold">Repotting Frequency:</span> Every ${plant.repotting_frequency_in_days} days</div>
+                <div><span class="font-bold">Soil Type:</span> ${plant.soil_type}</div>
+                </div>
+                <div>${plant.description}</div>
+                <button id="add-plant-to-schedule" class="text-white hover:bg-green-600  bg-green-900  px-8 p-2 rounded-full ease-in duration-300 hover:scale-110">Add to Schedule</button>
+            </div>
         `;
         const addPlantToScheduleButton = document.querySelector(
             '#add-plant-to-schedule'
--- client/js/components/search-results.js
+++ client/js/components/search-results.js
@@ -8,12 +8,16 @@ function renderSearchResults(searchQuery) {
         results.forEach((result) => {
             const item = document.createElement('div');
             item.innerHTML = `
-                <div id="search-result" onClick="renderPlant(${result.id})" class="flex flex-wrap bg-white w-96 py-8 m-auto justify-center">
-                    <h3 class="text-2xl"> ${result.name} </h3>
-                        <img src="${result.image_url}" height=200px width=200px ></img>
+                <div id="search-result" onClick="renderPlant(${result.id})" class="flex flex-wrap  w-2/3 py-8 px-6 m-auto justify-center gap-5 bg-white">
+                    <div id="search-result-container" class="flex flex-row gap-4 ">    
+                        <img src="${result.image_url}" class="rounded h-auto w-52 cursor-pointer"/>
+                    <div class="flex flex-col justify-center">
+                        <h3 class="text-2xl"> ${result.name} </h3>
                         <p>Latin name: <i> ${result.latin_name} </i></p>
                         <p> ${result.description}</p>
                         <a href="#####" class=" text-green-900">Learn more about ${result.name} here</a>
+                    </div>
+                    </div>
                 </div>
             `;
             resultsContainer.appendChild(item);
