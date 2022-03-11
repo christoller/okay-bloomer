b4c23371b4a9588006159777eaa9f27632fab1ec
christoller <tollernz@gmail.com>
2022-03-11 11:40:07 +1100

    Move location of error message on login and signup, css fixes, show some content when schedule is empty

--- client/js/components/sessions/login.js
+++ client/js/components/sessions/login.js
@@ -12,7 +12,8 @@ function renderLoginForm() {
                 <p class="font-bold my-2.5">Password:</p>
                 <input type="password" name="password" class="border-2 border-neutral-400/50 rounded"> 
                 <br/>
-                <button type="submit" class="mb-4 lg:mb-0 mt-4 px-8 py-0.5 bg-green-900 text-white rounded-full hover:bg-green-600 ease-in duration-300 hover:scale-110">Login</button>
+                <p class="error-location text-red-600 mt-1"></p>
+                <button type="submit" class="mb-4 lg:mb-0 mt-4 px-8 py-0.5 bg-green-900 text-white rounded-full hover:bg-green-600 ease-in duration-300 hover:scale-110 ">Login</button>
             </form>
         </div>
     `;
